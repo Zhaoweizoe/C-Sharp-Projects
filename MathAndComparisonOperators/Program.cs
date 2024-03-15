99ea496b2fcf6dee6a6158442d648bd1e764cfda
Zhaoweizoe <ukzhaowei@outlook.com>
2024-03-15 11:29:29 +0000

    update

---
+++ MathAndComparisonOperators/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MathAndComparisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print program title
+            Console.WriteLine("Anonymous Income Comparison Program");
+            // Gather information for Person 1
+            Console.WriteLine("Person 1");
+            Console.WriteLine("Hourly Rate?");
+            string hourlyRate1 = Console.ReadLine();
+            Console.WriteLine("Hours worked per week?");
+            string hours1 = Console.ReadLine();
+            int salary1 = Convert.ToInt32(hourlyRate1) * Convert.ToInt32(hours1) * 52;
+            // Gather information for Person 2
+            Console.WriteLine("Person 2");
+            Console.WriteLine("Hourly Rate?");
+            string hourlyRate2 = Console.ReadLine();
+            Console.WriteLine("Hours worked per week?");
+            string hours2 = Console.ReadLine();
+            int salary2 = Convert.ToInt32(hourlyRate2) * Convert.ToInt32(hours2) * 52;
+            // Display annual salary of Person 1
+            Console.WriteLine("Annual salary of Person1:");
+            Console.WriteLine(salary1);
+            // Display annual salary of Person 2
+            Console.WriteLine("Annual salary of Person2:");
+            Console.WriteLine(salary2);
+            // Check if Person 1 makes more money than Person 2
+            Console.WriteLine("Dose Person 1 make more money than Person 2?");
+            // Compare salaries of Person 1 and Person 2
+            bool isMore = salary1 > salary2;
+            // Print the result of the comparison
+            Console.WriteLine(isMore);
+            // Pause the console so the result can be seen before the program exits
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ MathChallenge/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace MathChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num1 = 2;
+            //int num2 = 3;
+            //int total = num1 + num2;
+            //Console.WriteLine(total);
+            //Console.ReadLine();
+
+            //int num1 = 5;
+            //int num2 = 3;
+            //int difference = num1 - num2;
+            //Console.WriteLine(difference);
+            //Console.ReadLine();
+
+            //int num1 = 5;
+            //int num2 = 3;
+            //int product = num1 * num2;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            //int num1 = 6;
+            //int num2 = 3;
+            //int quotient = num1 / num2;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            //string firstName = "Zoe";
+            //string lastName = "Wang";
+            //Console.WriteLine(firstName + lastName);
+            //Console.ReadLine();
+
+            //string firstName1 = "Zoe";
+            //int number = 1;
+            //Console.WriteLine(firstName1 + number);
+            //Console.ReadLine();
+
+            decimal num1 = 19.75488m;
+            int num2 = 6;
+
+            decimal total = num1 + num2;
+
+            Console.WriteLine(total);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
