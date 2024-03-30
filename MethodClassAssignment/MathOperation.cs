d9a2af6271b9c9075516eb5fbe00609caed61d81
Zhaoweizoe <ukzhaowei@outlook.com>
2024-03-30 16:50:29 +0000

    Method Class Assignment

---
+++ MethodClassAssignment/MathOperation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodClassAssignment
+{
+    public class MathOperation
+    {
+        // Create a void method that takes two integers as parameters
+        public void MyOperation(int num1, int num2)
+        {
+            // Perform a math operation on the first integer
+            int result = num1 * 5;
+            // Display the result of the math operation
+            Console.WriteLine("The result is" + result);
+            // Display the second integer to the screen
+            Console.WriteLine("The second integer is: " + num2);
+        }
+    }
+}
---
+++ MethodClassAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the MathOperation class
+            MathOperation mathOp = new MathOperation();
+            // Call the method in the class, passing in two numbers
+            mathOp.MyOperation(2, 19);
+            // Call the method in the class, specifying the parameters by name
+            mathOp.MyOperation(num1: 3, num2: 6);
+            Console.ReadLine();
+        }
+    }
+}
