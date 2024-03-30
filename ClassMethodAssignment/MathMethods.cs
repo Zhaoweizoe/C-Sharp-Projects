cd84e878c38c2ab0e7a082bb15eb1bcd8d2b60fc
Zhaoweizoe <ukzhaowei@outlook.com>
2024-03-30 19:26:37 +0000

    module 4E

---
+++ ClassMethodAssignment/MathMethods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodAssignment
+{
+    class MathMethods
+    {
+        //create a void method that outputs an integer. Have the method divide the data passed to it by 2.
+        public void DivideByTwo(int number)
+        {
+        double result = number / 2;
+        Console.WriteLine("The result of dividing by 2 is:"+result);
+        }
+        // Create a method with output parameters
+        public void DivideByTwoWithOutput(int number, out int result)
+        {
+            // Divide the data passed to the method by 2 and assign the result to the output parameter
+            result = number / 2;
+        }
+        // Overload the method to handle a double input
+        public void DivideByTwo(double number)
+        {
+            // Divide the data passed to the method by 2 and output the result
+            double result = number / 2;
+            Console.WriteLine("The result of dividing by 2 is:"+result);
+        }
+    }
+}
---
+++ ClassMethodAssignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassMethodAssignment
+{
+    // Declare a class to be static
+    static class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            // Instantiate the MathOperations class
+            MathMethods mathOp = new MathMethods();
+
+            // Prompt the user to enter a number
+            Console.Write("Enter a number: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            // Call the method on the entered number
+            
+            mathOp.DivideByTwo(userInput);
+
+
+
+            // Call the overloaded method with a double input
+            
+            mathOp.DivideByTwo(10.5);
+            
+
+
+            // Call the method with output parameters
+            int output;
+            mathOp.DivideByTwoWithOutput(userInput, out output);
+            Console.WriteLine("The result of dividing " + userInput + " by 2 using output parameters is: " + output);
+
+            Console.ReadLine();
+        }
+    }
+}
