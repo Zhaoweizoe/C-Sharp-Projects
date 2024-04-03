2a7caa97c06c0499a5e57aec693073dfd4c1e7ed
Zhaoweizoe <ukzhaowei@outlook.com>
2024-04-03 19:09:30 +0100

    Operators Assignment

---
+++ OperatorsAssignment/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperatorsAssignment
+{
+    // Employee class with Id, FirstName, and LastName properties
+    public class Employee
+    {
+        // Properties
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        //behavior: Constructor to initialize the properties
+        public Employee(int id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        // Overloading the "==" operator to check if two Employee objects refer to the same instance
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            // If both references are null, return true
+            if (ReferenceEquals(emp1, null) && ReferenceEquals(emp2, null))
+                return true;
+
+            // If one reference is null while the other isn't, return false
+            if (ReferenceEquals(emp1, null) || ReferenceEquals(emp2, null))
+                return false;
+
+            // Compare references
+            return ReferenceEquals(emp1, emp2);
+        }
+
+        // Overloading the "!=" operator (required for pairs of overloaded operators)
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            return !(emp1 == emp2);
+        }
+
+    }
+}
---
+++ OperatorsAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating some employee objects for demonstration
+            Employee emp1 = new Employee(1, "Ann", "Red");
+            Employee emp2 = new Employee(2, "Lily", "Green");
+            Employee emp3 = emp1; // Assigning emp1 to emp3, so they refer to the same object
+            Employee emp4 = emp2; // Assigning emp4 to emp2, so they refer to the same object
+
+            // Comparing employee objects using the overloaded "==" operator
+            Console.WriteLine("Comparing emp1 and emp2: " + (emp1 == emp2)); // Should be false
+            Console.WriteLine("Comparing emp1 and emp3: " + (emp1 == emp3)); // Should be true
+            Console.WriteLine("Comparing emp2 and emp4: " + (emp2 == emp4)); // Should be true
+            Console.WriteLine("Comparing emp2 and emp3: " + (emp2 == emp3)); // Should be false
+            Console.WriteLine("Comparing emp1 and emp4: " + (emp1 == emp4)); // Should be false
+
+            Console.ReadLine();
+        }
+    }
+}
