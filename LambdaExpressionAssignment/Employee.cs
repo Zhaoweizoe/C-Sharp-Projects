2701ebae06e2dea80d8e06e8a6a204595015a1be
Zhaoweizoe <ukzhaowei@outlook.com>
2024-04-04 10:53:45 +0100

    Lambda Expression Assignment

---
+++ LambdaExpressionAssignment/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LambdaExpressionAssignment
+{
+    //Create an Employee class with Id,First Name and Last Name properties
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+    }
+}
