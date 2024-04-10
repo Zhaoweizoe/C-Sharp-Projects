b4b71738921591fd0c0944f6847643add30d5f92
Zhaoweizoe <ukzhaowei@outlook.com>
2024-04-10 17:08:20 +0100

    Constructor Assignment

---
+++ ConstructorAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a const variable.
+            const string const1 = "Your name is:";
+            const string const2 = "Your favourite number is:";
+            Console.WriteLine("Enter your name of favourite number:");
+            //Create a variable using the keyword “var.”
+            var answer = Console.ReadLine();
+            if (float.TryParse(answer,out float x))
+            {
+                Console.WriteLine(const2);
+                Chain newChain = new Chain(x);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine(const1);
+                Chain newChain = new Chain(answer);
+                Console.ReadLine();
+            }
+        }
+    }
+}
