0b77965d6356b0011d75206d20a9ee76d304a88a
Zhaoweizoe <ukzhaowei@outlook.com>
2024-05-17 10:31:04 +0100

    a basic Entity Framework Code-First console application that creates a basic Student database with one student

---
+++ FinalAssignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace FinalAssignment
+{
+    class Progrom
+    {
+        static void Main(string[] args)
+        {
+
+            Student student = new Student
+            {
+                Id = 1,
+                FirstName = "Joe",
+                LastName = "White"
+            };
+
+            Console.WriteLine("Student ID: " + student.Id);
+            Console.WriteLine("First Name: " + student.FirstName);
+            Console.WriteLine("Last Name: " + student.LastName);
+        }
+        public class Student
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+        public class BloggingContext : DbContext
+        {
+            public DbSet<Student> Student { get; set; }
+            
+        }
+    }
+}
+
+
+
